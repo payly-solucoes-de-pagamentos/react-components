7b2ad2ef46fe92fac18a756d84209e724cfdc94d
Nikolas Freitas <nikolas.freitas@4all.com>
2019-05-17 20:04:40 -0300

    Add Story of modal

--- example/stories/index.stories.js
+++ example/stories/index.stories.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import {Modal} from 'payly-components-js';
+import Styled from 'styled-components';
+import { Modal } from 'payly-components';
+
+const Pzinho = Styled.p`
+  text-align: center;
+`;
 
 storiesOf("teste", module).
-add('testeDoAdd', () => <Modal active />);
+add('testeDoAdd', () => <Modal active ContentComponent={() => <Pzinho>Testezinho</Pzinho>} />);
