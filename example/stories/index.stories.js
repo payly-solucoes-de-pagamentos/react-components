5a63f7454132de22f9bd91aaf7957d098d00b8a4
Nikolas Freitas <nikolas.freitas@4all.com>
2019-06-10 17:59:06 -0300

    Add checkbox story

--- example/stories/index.stories.js
+++ example/stories/index.stories.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Styled from 'styled-components';
-import { Modal } from 'payly-components';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Styled from "styled-components";
+import { Modal, Checkbox } from "payly-components";
+
 
 const Pzinho = Styled.p`
   text-align: center;
 `;
 
-storiesOf("teste", module).
-add('testeDoAdd', () => <Modal active ContentComponent={() => <Pzinho>Testezinho</Pzinho>} />);
+storiesOf("teste", module)
+  .add("testeDoAdd", () => (
+    <Modal active ContentComponent={() => <Pzinho>Modal 1</Pzinho>} />
+  ))
+  .add("testeDoAdd2", () => (
+    <Checkbox />
+  ));
+
