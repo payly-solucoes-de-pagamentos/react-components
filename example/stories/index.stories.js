273711c0a645398e9b46c777bc3bfb7d1840522c
Nikolas Freitas <nikolasbvf@gmail.com>
2019-06-13 17:39:19 -0300

    :ok:

--- example/stories/index.stories.js
+++ example/stories/index.stories.js
@@ -3,16 +3,14 @@ import { storiesOf } from "@storybook/react";
 import Styled from "styled-components";
 import { Modal, Checkbox } from "payly-components";
 
-
 const Pzinho = Styled.p`
   text-align: center;
 `;
 
 storiesOf("teste", module)
   .add("testeDoAdd", () => (
-    <Modal active ContentComponent={() => <Pzinho>Modal 1</Pzinho>} />
+    <Modal active>
+      <Pzinho>Modal 1</Pzinho>
+    </Modal>
   ))
-  .add("testeDoAdd2", () => (
-    <Checkbox />
-  ));
-
+  .add("testeDoAdd2", () => <Checkbox />);
