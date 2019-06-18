84027a71d6df3cb03653d5a5103c58abe3c0165e
Nikolas Freitas <nikolas.freitas@4all.com>
2019-06-18 20:15:30 -0300

    :fire: remove dates components of lib due bugs

---
+++ example/stories/react-date.story.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useState } from "react";
+import { storiesOf } from "@storybook/react";
+import { DateCalendar } from "payly-components";
+
+const DatePickerComponent = () => {
+  const [initialDate, setInitialDate] = useState(null);
+  const [finalDate, setFinalDate] = useState(null);
+  const [focus, setFocus] = useState(undefined);
+
+  const dateChanger = ({ startDate, endDate }) => {
+    console.log(startDate);
+
+    setInitialDate(startDate);
+    setFinalDate(endDate);
+  };
+
+  return (
+    <div>
+      <DateCalendar
+        label="Data de Início"
+        startDate={initialDate}
+        startDateId="startAtdateBegin"
+        endDate={finalDate}
+        endDateId="endAtdateBegin"
+        onChange={dateChanger}
+        focusedInput={focus}
+        onFocus={setFocus}
+        disabled={false}
+        hasWeekDays={false}
+        placeholder="Selecione"
+        width="167px"
+        openDirection="down"
+      />
+    </div>
+  );
+};
+
+// storiesOf("date calendar").add("simplest implementation", () => (
+//   <DatePickerComponent />
+// ));
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ export { default as Checkbox } from "./components/Checkbox";
 export { default as Tooltip } from "./components/Tooltip";
 export { default as SentFeedback } from "./components/SentFeedback";
 export { default as Table } from "./components/Table";
-export { default as DateCalendar } from "./components/DateCalendar";
-export { default as RangeCalendar } from "./components/RangeCalendar";
+// export { default as DateCalendar } from "./components/DateCalendar";
+// export { default as RangeCalendar } from "./components/RangeCalendar";
