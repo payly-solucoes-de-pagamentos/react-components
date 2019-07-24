f02a5e371ab01cb1e0d124fac84b0258766c44fc
Nikolas Freitas <nikolas.freitas@4all.com>
2019-07-24 20:35:32 -0300

    :sparkles: Update storybook due upgrades in RadioButton component

--- stories/radio.story.js
+++ stories/radio.story.js
@@ -7,27 +7,35 @@ import RadioButton from '../src/components/RadioButton';
 storiesOf('Radio button')
   .add('Default', () => (
     <div style={{ background: 'grey', paddingLeft: '15px' }}>
-      <RadioButton name="yup" identifier="t1">
+      <RadioButton name="yup" identifier="t1" callback={() => alert('click')>
         Click aqui
       </RadioButton>
     </div>
   ))
   .add('With two radios', () => (
     <div style={{ background: 'grey', paddingLeft: '15px' }}>
-      <RadioButton name="yup" identifier="t1">
+      <RadioButton
+        name="yup"
+        identifier="t1"
+        callback={() => alert('click no primeiro')}
+      >
         Click aqui
       </RadioButton>
-      <RadioButton name="yup" identifier="t2">
+      <RadioButton
+        name="yup"
+        identifier="t2"
+        callback={() => alert('click no segundo')}
+      >
         ou aqui
       </RadioButton>
     </div>
   ))
   .add('With two radios and default seted', () => (
     <div style={{ background: 'grey', paddingLeft: '15px' }}>
-      <RadioButton name="yup" identifier="t1" isDefault>
+      <RadioButton name="yup" identifier="t1" isDefault callback={() => alert('click no primeiro')}>
         Click aqui
       </RadioButton>
-      <RadioButton name="yup" identifier="t2">
+      <RadioButton name="yup" identifier="t2" callback={() => alert('click no segundo')}>
         ou aqui
       </RadioButton>
     </div>
