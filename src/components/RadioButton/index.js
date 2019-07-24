4fba3248d9ed1865f06fc1794d251dd5c9b263b5
Nikolas Freitas <nikolas.freitas@4all.com>
2019-07-24 20:32:24 -0300

    :sparkles: Make the Radio button ready to homologation

--- src/components/RadioButton/index.js
+++ src/components/RadioButton/index.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import * as Styled from './style';
 
-const RadioButton = ({ name, children, identifier, isDefault }) => (
-  <Styled.Container>
-    <Styled.RadioInput
-      defaultChecked={isDefault}
-      id={identifier}
-      name={name}
-      type="radio"
-    />
-    <Styled.Box htmlFor={identifier}>
-      <Styled.RadioHolder />
-      <Styled.Text>{children}</Styled.Text>
-    </Styled.Box>
-  </Styled.Container>
-);
+const RadioButton = ({ name, children, identifier, isDefault, callback }) => {
+  const onChangeHandler = e => {
+    if (e.target.checked) {
+      callback();
+    }
+  };
+  return (
+    <Styled.Container>
+      <Styled.RadioInput
+        defaultChecked={isDefault}
+        id={identifier}
+        name={name}
+        onChange={onChangeHandler}
+        type="radio"
+      />
+      <Styled.Box htmlFor={identifier}>
+        <Styled.RadioHolder />
+        <Styled.Text>{children}</Styled.Text>
+      </Styled.Box>
+    </Styled.Container>
+  );
+};
 
 RadioButton.propTypes = {
   name: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
   identifier: PropTypes.string.isRequired,
+  callback: PropTypes.func.isRequired,
   isDefault: PropTypes.bool,
 };
 
