a5582b86e1354ea2684100e8892dba69b20ef021
Daniel Arenhart <arenhart.daniel@gmail.com>
2019-08-22 17:34:39 -0300

    Recover Label component

---
+++ src/components/Label/styles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Label = styled.div`
+  height: 24px;
+  background: ${props => props.background};
+  width: ${props => props.width};
+  color: ${props => props.color || theme.text.mainColor};
+  border-radius: 22.5px;
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
+  align-items: center;
+  font-size: 12px;
+  font-weight: 600;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+`;
+
+export const IconContainer = styled.div`
+  margin-left: 8px;
+  margin-right: 8px;
+  margin-top: 2px;
+`;
+
+export const DangerContainer = styled.div`
+  align-self: center;
+  justify-self: center;
+  margin-left: 5px;
+  font-size: 12px;
+  font-weight: 600;
+  font-style: normal;
+  font-stretch: normal;
+  letter-spacing: normal;
+  color: #d0021b;
+
+  & span {
+    margin-left: 3px;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ export { default as Table } from './components/Table';
 export { default as Balance } from './components/Balance';
 export { default as Button } from './components/Button';
 export { default as Badge } from './components/Badge';
-export { default as Label } from './components/Badge'; /* deprecated Label */
+export { default as Label } from './components/Label';
 export { default as Toast } from './components/Toast';
 export { default as RadioButton } from './components/RadioButton';
